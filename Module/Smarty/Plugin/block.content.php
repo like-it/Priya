5b1ed8de4aed989331fccb8f69274d409012bbca
Remco van der Velde <r.vandervelde@newcomresearch.nl>
2017-02-21 15:11:47 +0100

    rename @category

--- Module/Smarty/Plugin/block.content.php
+++ Module/Smarty/Plugin/block.content.php
@@ -6,7 +6,7 @@
  *
  * @package Smarty
  * @subpackage PluginsBlock
- * @category block_json
+ * @category block_content
  *
  * @description
  * Smarty plugin
@@ -31,58 +31,58 @@ function smarty_block_content($params, $content, $template, &$repeat)
                 $assign = (string) $_val;
             break;
             case 'trim':
-            	$trim = (string) $_val;
+                $trim = (string) $_val;
             break;
             case 'search':
-            	$search = (array) $_val;
+                $search = (array) $_val;
             break;
             case 'replace':
-            	$replace = (array) $_val;
+                $replace = (array) $_val;
             break;
             default:
                 trigger_error("content_block: unknown attribute '$_key'");
         }
     }
     if($trim=='html'){
-    	$content = trim($content, "\r\n\s\t");
-    	$data = explode('<', $content);
-    	foreach ($data as $nr => $row){
-    		$dataRow = explode('>', $row);
-    		if(count($dataRow)>=2){
-    			foreach ($dataRow as $dataRowNr => $dataR){
-    				$tmp = str_replace($search, $replace, $dataR);
-    				if(empty($tmp)){
-    					$dataRow[$dataRowNr] = '';
-    				}
-    			}
-    			$data[$nr] = implode('>', $dataRow);
-    		}
-    	}
-    	$content = implode('<', $data);
-	}
-	$priya = '<!-- <priya-' . str_replace('_', '-', $assign);
-	$class = array();
+        $content = trim($content, "\r\n\s\t");
+        $data = explode('<', $content);
+        foreach ($data as $nr => $row){
+            $dataRow = explode('>', $row);
+            if(count($dataRow)>=2){
+                foreach ($dataRow as $dataRowNr => $dataR){
+                    $tmp = str_replace($search, $replace, $dataR);
+                    if(empty($tmp)){
+                        $dataRow[$dataRowNr] = '';
+                    }
+                }
+                $data[$nr] = implode('>', $dataRow);
+            }
+        }
+        $content = implode('<', $data);
+    }
+    $priya = '<!-- <priya-' . str_replace('_', '-', $assign);
+    $class = array();
 
-	$search_class = trim('-' . str_replace(array(" ", "\t", "\n", "\r", "\r\n"),'',implode('-', $search)), '-');
-	$replace_class = trim('-' . str_replace(array(" ", "\t", "\n", "\r", "\r\n"),'',implode('-', $replace)), '-');
+    $search_class = trim('-' . str_replace(array(" ", "\t", "\n", "\r", "\r\n"),'',implode('-', $search)), '-');
+    $replace_class = trim('-' . str_replace(array(" ", "\t", "\n", "\r", "\r\n"),'',implode('-', $replace)), '-');
 
 
 
-	if(empty($trim)){
-		$class[] = 'html-search' . $search_class;
-		$class[] = 'html-replace' . $replace_class;
-	} else {
-		$class[] = $trim . '-trim';
-		$class[] = $trim . '-search' . $search_class;
-		$class[] = $trim . '-replace' . $replace_class;
-	}
-	$priya .= ' class="' . implode(' ', $class) . '">  //-->' . "\r\n";
-	$priya .= $content;
-	$priya .= '<!-- </priya-' . str_replace('_', '-', $assign) . '> //-->' . "\r\n";
+    if(empty($trim)){
+        $class[] = 'html-search' . $search_class;
+        $class[] = 'html-replace' . $replace_class;
+    } else {
+        $class[] = $trim . '-trim';
+        $class[] = $trim . '-search' . $search_class;
+        $class[] = $trim . '-replace' . $replace_class;
+    }
+    $priya .= ' class="' . implode(' ', $class) . '">  //-->' . "\r\n";
+    $priya .= $content;
+    $priya .= '<!-- </priya-' . str_replace('_', '-', $assign) . '> //-->' . "\r\n";
 
     if($assign){
-    	$template->assign($assign, $priya);
+        $template->assign($assign, $priya);
     } else {
-    	return $priya;
+        return $priya;
     }
 }
