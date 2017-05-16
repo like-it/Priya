3f22d04b14414f61bb79ec8aa912de42ca45c53b
remco.pc <remco.pc@outlook.com>
2017-05-16 18:32:55 +0200

    rename round

---
+++ Module/Parser/Function/Function.Math_round.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @author 		Remco van der Velde
+ * @since 		2017-04-20
+ * @version		1.0
+ * @changeLog
+ * 	-	all
+ */
+
+function function_math_round($value=null, $argumentList=array(), $parser=null){
+    if(!is_array($argumentList)){
+        $argumentList = (array) $argumentList;
+    }
+    $val = array_shift($argumentList);
+    $precision = count($argumentList) >= 1 ? array_shift($argumentList) : 0;
+    $mode= count($argumentList) >= 1 ? array_shift($argumentList) : PHP_ROUND_HALF_UP;
+
+    return round($val, $precision, $mode);
+}
