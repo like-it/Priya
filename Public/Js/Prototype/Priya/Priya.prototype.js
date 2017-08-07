1151d736b05f86d836c34463c0a40b185ec50250
remco.pc <remco.pc@outlook.com>
2017-08-07 21:29:54 +0200

    added namespace

--- Public/Js/Prototype/Priya/Priya.prototype.js
+++ Public/Js/Prototype/Priya/Priya.prototype.js
@@ -38,8 +38,22 @@ priya.prototype.run = function (data){
     return element;
 }
 
-priya.prototype.dom = function(data){
+priya.prototype.dom = function (data){
     return this.init(data);
+}
+
+priya.prototype.namespace = function (namespace) {
+    var object = this, tokens = namespace.split("."), token;
+    while (tokens.length > 0) {
+        token = tokens.shift();
+
+        if (typeof object[token] === "undefined") {
+            object[token] = {};
+        }
+
+        object = object[token];
+    }
+    return object;
 }
 
 priya.prototype.debug = function(data){
