abc9c3b99a13177c6f509525a646d1893b323739
remco.pc <remco.pc@outlook.com>
2017-11-04 21:51:49 +0100

    bugfix  public

--- Module/Cli/Application/Locate/Template/Locate.tpl.php
+++ Module/Cli/Application/Locate/Template/Locate.tpl.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * @author 		Remco van der Velde
- * @since 		2016-11-07
- * @version		1.0
+ * @author         Remco van der Velde
+ * @since         2016-11-07
+ * @version        1.0
  * @changeLog
- * 	-	all
+ *     -    all
  * @note
  *  - In Smarty bash coloring isn't working.
  */
