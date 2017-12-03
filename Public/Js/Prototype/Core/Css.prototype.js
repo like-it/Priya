58fae9e04a1af8638e13ff8b246bd99c38ad3c10
remco.pc <remco.pc@outlook.com>
2017-12-03 20:38:15 +0100

    added computeStyle (for computing middle & center)

--- Public/Js/Prototype/Core/Css.prototype.js
+++ Public/Js/Prototype/Core/Css.prototype.js
@@ -11,18 +11,32 @@ priya.css = function(attribute, value){
     if(attribute == 'has'){
         return !!this.style[value];
     }
-    if(attribute == 'delete'){
+    else if(attribute == 'delete'){
         this.style[value] = '';
     }
     if(this.is_nodeList(this)){
         var index;
         for(index=0; index < this.length; index++){
             var node = this[index];
+            value = node.computeStyle(attribute, value);
             node.style[attribute] = value;
         }
     } else {
+        value = this.computeStyle(attribute, value);
         this.style[attribute] = value;
     }
+}
+
+priya.computeStyle = function(attribute, value){
+    if(attribute == 'top' && value=='middle'){
+        var height = parseInt(this.css('height'));
+        value = 'calc(50% - ' + (height /2) + 'px)';
+    }
+    else if(attribute == 'left' && value=='center'){
+        var width = parseInt(this.css('width'));
+        value = 'calc(50% - ' + (width /2) + 'px)';
+    }
+    return value;
 }
 
 priya.computedStyle = function(attribute){
