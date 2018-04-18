afd7e42652049deb678200702998d92899f0e64e
remco.pc <remco.pc@outlook.com>
2018-04-18 02:53:00 +0200

    nice

--- Module/Parse/Class/Priya.class.php
+++ Module/Parse/Class/Priya.class.php
@@ -39,6 +39,9 @@ class Priya extends Core {
             if(isset($explode[1])){
                 $content = explode(Tag::OPEN . Priya::CLOSE . TAG::CLOSE, $explode[1], 2);
             }
+            $code = Priya::tag($content[0]);
+            var_dump($code);
+            die;
             $part = explode(Priya::NEWLINE, $content[0]);
             $program = array();
             $mask = ' ' . Tag::OPEN . Tag::CLOSE;
@@ -86,6 +89,206 @@ class Priya extends Core {
             $string = implode('', $explode);
         }
         return $string;
+    }
+
+    public static function tag($string=''){
+        //add tag['split'] so we only split once...
+        $previous_char = '';
+        $next = null;
+        $next_next = null;
+        $parse = false;
+        $no_parse = false;
+        $variable = false;
+        $counter = 0;
+        $set_depth = 0;
+        $comment_depth = 0;
+        $statement = array();
+        $statement[$counter]['string'] = '';
+        $split = str_split($string);
+        $count = count($split);        
+        foreach($split as $nr => $char){     
+            if($comment_depth > 0){
+                if(isset($split[$nr + 1])){
+                    $next = $split[$nr + 1];
+                } else {
+                    $next = null;
+                }
+                if(isset($split[$nr + 2])){
+                    $next_next = $split[$nr + 2];
+                } else {
+                    $next_next = null;
+                }
+                if(
+                    $variable === false &&
+                    $char == '*' &&
+                    $next = '/'
+                ){
+                    $comment_depth--;
+                    continue;
+                }            
+            }
+            if(
+                $char == '"' &&
+                $parse === true &&
+                $no_parse == false &&
+                $previous_char !== '\\'
+            ){
+                $statement[$counter]['string'] .= $char;
+                $parse = false; //no counter++
+                if($nr < $count){
+                    $counter++;
+                }
+                $previous_char = $char;
+                continue;
+            }
+            elseif(
+                $char == '"' &&
+                $parse === false &&
+                $no_parse === false &&
+                $previous_char !== '\\'
+            ){
+                if(!empty($statement[$counter]['string'])){
+                    $counter++;
+                    $statement[$counter]['string'] = '';
+                }
+                if(!isset($statement[$counter])){
+                    $statement[$counter]['string'] = '';
+                }
+                $statement[$counter]['string'] .= $char;
+                $statement[$counter]['type'] = Tag::TYPE_PARSER;
+                $parse = true;
+                $variable = false;
+                $previous_char = $char;
+                continue;
+            }
+            elseif(
+                $char == '\'' &&
+                $no_parse === true &&
+                $parse === false &&
+                $previous_char !== '\\'
+            ){
+                $statement[$counter]['string'] .= $char;
+                $no_parse = false; //no counter++
+                if($nr < $count){
+                    $counter++;
+                }
+                $previous_char = $char;
+                continue;
+            }
+            elseif(
+                $char == '\'' &&
+                $no_parse === false &&
+                $parse === false &&
+                $previous_char !== '\\'
+            ){
+                if(!empty($statement[$counter]['string'])){
+                    $counter++;
+                    $statement[$counter]['string'] = '';
+                }
+                if(!isset($statement[$counter])){
+                    $statement[$counter]['string'] = '';
+                }
+                $statement[$counter]['string'] .= $char;
+                $statement[$counter]['type'] = Tag::TYPE_STRING;
+                $no_parse = true;
+                $variable = false;
+                $previous_char = $char;
+                continue;
+            }
+            elseif(
+                $parse === true &&
+                $no_parse === false &&
+                $variable === false
+                
+            ){
+                $statement[$counter]['string'] .= $char;
+                $previous_char = $char;
+                continue;
+            }
+            elseif(
+                $no_parse === true &&
+                $parse === false &&
+                $variable === false
+            ){
+                $statement[$counter]['string'] .= $char;
+                $previous_char = $char;
+                continue;
+            }            
+            if(isset($split[$nr + 1])){
+                $next = $split[$nr + 1];
+            } else {
+                $next = null;
+            }
+            if(isset($split[$nr + 2])){
+                $next_next = $split[$nr + 2];
+            } else {
+                $next_next = null;
+            }
+            if(
+                $parse === false &&
+                $no_parse === false
+            ){
+                if(
+                    $variable === false &&
+                    $char == ';'
+                ){
+                    if(!empty($statement[$counter]['string'])){                        
+                        $counter++;
+                    }
+                    $statement[$counter]['string'] = $char;
+                    $statement[$counter]['type'] = TAG::TYPE_SEMI_COLON;
+                    $counter++;
+                    continue;
+                }
+                elseif(
+                    $variable === false && 
+                    $char == '/' && 
+                    $next = '*'
+                ){
+                    $comment_depth++;
+                    continue;
+                }
+                elseif(
+                    $variable === false &&
+                    $char == '*' &&
+                    $next = '/'
+                ){
+                    $comment_depth--;
+                    continue;
+                }
+                
+                
+                else {
+                    if(!isset($statement[$counter]['string'])){
+                        $statement[$counter]['string'] = $char;
+                    } else {
+                        $statement[$counter]['string'] .= $char;
+                    }
+                }
+            } else {
+                if(!isset($statement[$counter]['string'])){
+                    $statement[$counter]['string'] = $char;
+                } else {
+                    $statement[$counter]['string'] .= $char;
+                }
+            }
+        }
+        $result = array();
+        $code = '';
+        foreach($statement as $counter => $part){
+            if(isset($part['type']) && $part['type'] == TAG::TYPE_SEMI_COLON){
+                $result[] = '{' . trim($code) . '}';
+                $code = '';                
+            } else {
+                $code .= $part['string'];
+            }
+        }
+        var_dump($result);                      
+        
+        
+        
+//                 var_dump($statement);
+        die;
     }
 
     public static function exectute($tag=array(), $attribute='', $parser=null){
