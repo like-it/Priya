ae081563748249e51b9ca621e8c2a54fd191f0c4
remco.pc <remco.pc@outlook.com>
2018-04-25 00:22:55 +0200

    added licence

--- Module/Cli/Application/Help/Template/Help.tpl.php
+++ Module/Cli/Application/Help/Template/Help.tpl.php
@@ -17,6 +17,7 @@ echo PHP_EOL;
 echo 'Priya <route> <parameter(s)>' . PHP_EOL;
 echo "  " . 'help                           (Priya help)' . PHP_EOL;
 echo "  " . 'version                        (Priya version)' . PHP_EOL;
+echo "  " . 'license                        (Priya license)' . PHP_EOL;
 echo "  " . 'locate <route>                 (this will list all available locations of the class <route> name)' . PHP_EOL;
 echo "  " . 'route --list                   (this will show available cli routes)' . PHP_EOL;
 echo "  " . 'route --list-all               (this will show all available routes)' . PHP_EOL;
@@ -25,23 +26,3 @@ echo "  " . 'cache/clear                    (clear cache)' . PHP_EOL;
 echo "  " . 'config                         (current configuration in json format)' . PHP_EOL;
 echo "  " . 'parser                         (generates a parser file (speed up parsing))' . PHP_EOL;
 echo "  " . 'javascript create              (generates the javascript source)' . PHP_EOL;
-/**
- * get <install> <update> <upgrade>  (like apt-get) so ...
- * task <source> a json task file...
- *     - {route('task-<name>',[attributes])}
- */
-/*
-echo "  " . 'install                        (install Priya)' . PHP_EOL;
-echo "  " . 'update --version=<version>       (update Priya to latest version)' . PHP_EOL;
-echo "  " . 'package                        (create a package)' . PHP_EOL;
-echo "  " . 'build <package> <target>      (build Priya)' . PHP_EOL;
-echo "  " . 'help                        (Priya help)' . PHP_EOL;
-echo "  " . 'push                        (push Priya to server)' . PHP_EOL;
-echo "  " . 'pull                        (pull Priya from server)' . PHP_EOL;
-echo "  " . 'restore create                 (this will create a restore point)' . PHP_EOL;
-echo "  " . 'restore --list                 (this will show available restore points)' . PHP_EOL;
-echo "  " . 'route --list                   (this will show available cli routes)' . PHP_EOL;
-echo "  " . 'route --list-all               (this will show all available routes)' . PHP_EOL;
-echo "  " . 'test <class> <file>             (this will test the <class> with <file>)' . PHP_EOL;
-echo "  " . 'locate <route>             (this will list all available locations of the class <route> name)' . PHP_EOL;
-*/
