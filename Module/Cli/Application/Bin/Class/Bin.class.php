cbc5f219fdc72c08634255cbee7497fea4468f9b
remco.pc <remco.pc@outlook.com>
2018-04-25 04:21:24 +0200

    cleanup

--- Module/Cli/Application/Bin/Class/Bin.class.php
+++ Module/Cli/Application/Bin/Class/Bin.class.php
@@ -20,51 +20,6 @@ class Bin extends Cli {
         $this->data('response', 'binary');
         $this->data('binary', Bin::response($this));
         $this->data('response', 'create');
-        echo Bin::response($this);
-
-        die;
-
-        var_dump($this->data('module.dir.data'));
-        die;
-
-
-        if($this->parameter('clear')){
-            Cache::clear($this, 'application');
-            Cache::clear($this, 'smarty');
-        }
-        $this->data('delete', 'response');
-        return Cache::response($this);
-        return $this->result('cli');
-    }
-
-    public static function clear($object, $type=''){
-        $object->data('response', $type);
-        switch ($type){
-            case 'smarty':
-                echo Cache::response($object);
-                return Cache::clearSmarty($object);
-            case 'application':
-                echo Cache::response($object);
-                return Cache::clearApplication($object);
-            break;
-        }
-    }
-
-    private static function clearApplication($object){
-        $url = $object->data('priya.dir.cache');
-        $dir = new Dir();
-        return $dir->delete($url);
-    }
-
-    private static function clearSmarty($object){
-        $url =
-            $object->data('priya.dir.module') .
-              'Smarty' .
-              Application::DS .
-              'Data' .
-              Application::DS
-          ;
-        $dir = new Dir();
-        return $dir->delete($url);
+        return Bin::response($this);
     }
 }
