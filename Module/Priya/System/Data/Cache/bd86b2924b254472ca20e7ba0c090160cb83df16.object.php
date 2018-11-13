4abad6f63ba102471d34dd2b0a1a3c3d373c9941
Remco van der Velde <remco.pc@outlook.com>
2018-11-13 15:58:10 +0200

    some cache examples from cli: priya system monitor

---
+++ Module/Priya/System/Data/Cache/bd86b2924b254472ca20e7ba0c090160cb83df16.object.php
@@ -0,0 +1,201 @@
+<?php $read = (object)(array(
+   'Process' => 
+  (object)(array(
+     'user' => 
+    array (
+      0 => 
+      (object)(array(
+         'root' => 'Priya.User.priya',
+      )),
+      1 => 
+      (object)(array(
+         'www-data' => 'Priya.User.gui',
+      )),
+      2 => 
+      (object)(array(
+         'memcache' => 'Priya.User.memory',
+      )),
+      3 => 
+      (object)(array(
+         'system' => 'Priya.User.system',
+      )),
+    ),
+     'command' => 
+    array (
+      0 => 
+      (object)(array(
+         'php' => 'Processor',
+      )),
+      1 => 
+      (object)(array(
+         'php5' => 'Processor',
+      )),
+      2 => 
+      (object)(array(
+         'php7' => 'Processor',
+      )),
+      3 => 
+      (object)(array(
+         'php7.0' => 'Processor',
+      )),
+      4 => 
+      (object)(array(
+         'php7.1' => 'Processor',
+      )),
+      5 => 
+      (object)(array(
+         'php7-fpm' => 'Processor',
+      )),
+      6 => 
+      (object)(array(
+         'apache2' => 'Host',
+      )),
+      7 => 
+      (object)(array(
+         'memcached' => 'Memory',
+      )),
+      8 => 
+      (object)(array(
+         'init' => 'Init',
+      )),
+      9 => 
+      (object)(array(
+         'top' => 'System.Monitor',
+      )),
+      10 => 
+      (object)(array(
+         'sh' => 'Shell',
+      )),
+      11 => 
+      (object)(array(
+         'firefox-esr' => 'Priya.Gui',
+      )),
+    ),
+     'load' => 
+    (object)(array(
+       'average' => 
+      (object)(array(
+         '1' => '0.52',
+         '2' => '0.58',
+         '3' => '0.59',
+      )),
+    )),
+  )),
+   'Task' => 
+  (object)(array(
+     'total' => '7',
+     'running' => '1',
+     'sleeping' => '6',
+     'stopped' => '0',
+     'zombie' => '0',
+     'nodeList' => 
+    array (
+      1 => 
+      array (
+        'user' => 'priya',
+        'pr' => '20',
+        'ni' => '0',
+        'virt' => '10588',
+        'res' => '300',
+        'shr' => '292',
+        's' => 'S',
+        'cpu' => '0.0',
+        'mem' => '0.0',
+        'time' => '0:00.21',
+        'command' => 'Init',
+        'jid' => '1',
+      ),
+      2 => 
+      array (
+        'user' => 'rem',
+        'pr' => '20',
+        'ni' => '0',
+        'virt' => '28412',
+        'res' => '2308',
+        'shr' => '2192',
+        's' => 'S',
+        'cpu' => '0.0',
+        'mem' => '0.1',
+        'time' => '0:01.21',
+        'command' => 'Bash',
+        'jid' => '2',
+      ),
+      120 => 
+      array (
+        'user' => 'rem',
+        'pr' => '20',
+        'ni' => '0',
+        'virt' => '39072',
+        'res' => '672',
+        'shr' => '640',
+        's' => 'S',
+        'cpu' => '0.0',
+        'mem' => '0.0',
+        'time' => '0:00.03',
+        'command' => 'Priya',
+        'jid' => '120',
+      ),
+      121 => 
+      array (
+        'user' => 'rem',
+        'pr' => '20',
+        'ni' => '0',
+        'virt' => '110904',
+        'res' => '13108',
+        'shr' => '13040',
+        's' => 'S',
+        'cpu' => '0.0',
+        'mem' => '0.3',
+        'time' => '1:46.70',
+        'command' => 'Processor',
+        'jid' => '121',
+      ),
+      2316 => 
+      array (
+        'user' => 'rem',
+        'pr' => '20',
+        'ni' => '0',
+        'virt' => '28148',
+        'res' => '3420',
+        'shr' => '3204',
+        's' => 'S',
+        'cpu' => '0.0',
+        'mem' => '0.1',
+        'time' => '0:00.16',
+        'command' => 'Bash',
+        'jid' => '2316',
+      ),
+      11250 => 
+      array (
+        'user' => 'rem',
+        'pr' => '20',
+        'ni' => '0',
+        'virt' => '121564',
+        'res' => '660',
+        'shr' => '628',
+        's' => 'S',
+        'cpu' => '0.0',
+        'mem' => '0.0',
+        'time' => '0:00.01',
+        'command' => 'Shell',
+        'jid' => '11250',
+      ),
+      11251 => 
+      array (
+        'user' => 'rem',
+        'pr' => '20',
+        'ni' => '0',
+        'virt' => '139052',
+        'res' => '1880',
+        'shr' => '1420',
+        's' => 'R',
+        'cpu' => '0.0',
+        'mem' => '0.0',
+        'time' => '0:00.05',
+        'command' => 'System.Monitor',
+        'jid' => '11251',
+      ),
+    ),
+  )),
+   'time' => '15:58:09',
+));
